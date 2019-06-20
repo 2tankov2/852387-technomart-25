07409add242f9d6865086e8d1ca4fe78e4673096
2tankov2 <domino-82@mail.ru>
2019-06-20 13:20:15 +0300

    минификация

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
-
-
 var link = document.querySelector(".contacts-button");
-
 var popup = document.querySelector(".modal-contacts");
-if  (popup) {
-var btnClose = popup.querySelector(".modal-close");
 
-var form = popup.querySelector("form");
-var login = popup.querySelector("[name=name]");
-var email = popup.querySelector("[name=email]");
+if  (popup) {
+  var btnClose = popup.querySelector(".modal-close");
+  var form = popup.querySelector("form");
+  var login = popup.querySelector("[name=name]");
+  var email = popup.querySelector("[name=email]");
 }
+
 var isStorageSupport = true;
 var storage = "";
 
@@ -20,29 +18,29 @@ try {
 }
 
 if (link) {
-link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.add("modal-show");
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
 
-  if (storage) {
-    login.value = storage;
-    email.focus();
-  } else {
+    if (storage) {
+      login.value = storage;
+      email.focus();
+    } else {
       login.focus();
     }
-});
+  });
 }
 
 if (btnClose) {
   btnClose.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
-    popup.classList.remote("modal-error");
+    popup.classList.remove("modal-error");
   });
 }
 
 if (form) {
-form.addEventListener("submit", function (evt) {
+  form.addEventListener("submit", function (evt) {
   if (!login.value || !email.value) {
     evt.preventDefault();
     popup.classList.remove("modal-error");
@@ -52,8 +50,8 @@ form.addEventListener("submit", function (evt) {
       if (isStorageSupport) {
         localStorage.setItem("login", login.value);
       }
-  }
-});
+    }
+  });
 }
 
 window.addEventListener("keydown", function (evt) {
@@ -69,21 +67,23 @@ window.addEventListener("keydown", function (evt) {
 var mapLink = document.querySelector(".map-button");
 
 var mapPopup = document.querySelector(".modal-map");
-if (mapClose) {
-var mapClose = mapPopup.querySelector(".modal-close");
+
+if (mapPopup) {
+  var mapClose = mapPopup.querySelector(".modal-close");
 }
+
 if (mapLink) {
-mapLink.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  mapPopup.classList.add("modal-show");
-});
+  mapLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+  });
 }
 
 if (mapClose) {
-mapClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  mapPopup.classList.remove("modal-show");
-});
+  mapClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+  });
 }
 
 var buyLink = document.querySelectorAll(".buy");
@@ -92,7 +92,6 @@ var buyPopup = document.querySelector(".modal-cart-form");
 var buyClose = buyPopup.querySelector(".modal-close");
 
 for (var i = 0; i < buyLink.length; i++) {
-
   buyLink[i].addEventListener("click", function (evt) {
     evt.preventDefault();
     buyPopup.classList.add("modal-show");
@@ -109,38 +108,38 @@ var buttonServices = document.querySelectorAll(".service-item");
 
 var contentServices = document.querySelectorAll(".service-content");
 
-if (buttonServices[i]) {
-buttonServices[0].addEventListener("click", function (evt) {
+if (buttonServices[0]) {
+  buttonServices[0].addEventListener("click", function (evt) {
   //сброс для всех
   for (var j=0; j<buttonServices.length; j++) {
       buttonServices[j].classList.remove("service-item--active");
       contentServices[j].classList.add("slider-item--close");
   }
-
    buttonServices[0].classList.add("service-item--active");
    contentServices[0].classList.remove("slider-item--close");
-});
+  });
+}
 
-buttonServices[1].addEventListener("click", function (evt) {
+if (buttonServices[1]) {
+  buttonServices[1].addEventListener("click", function (evt) {
   //сброс для всех
   for (var j=0; j<buttonServices.length; j++) {
       buttonServices[j].classList.remove("service-item--active");
       contentServices[j].classList.add("slider-item--close");
   }
-
    buttonServices[1].classList.add("service-item--active");
    contentServices[1].classList.remove("slider-item--close");
-});
+  });
+}
 
-
-buttonServices[2].addEventListener("click", function (evt) {
+if (buttonServices[2]) {
+  buttonServices[2].addEventListener("click", function (evt) {
   //сброс для всех
   for (var j=0; j<buttonServices.length; j++) {
       buttonServices[j].classList.remove("service-item--active");
       contentServices[j].classList.add("slider-item--close");
   }
-
    buttonServices[2].classList.add("service-item--active");
    contentServices[2].classList.remove("slider-item--close");
-});
+  });
 }
